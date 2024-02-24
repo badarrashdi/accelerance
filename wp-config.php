471b44f88f25f8d3095a7867a78edb7a60800b04
Badar Rashdi <badar.rashdi@gmail.com>
2024-02-24 20:11:23 +0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -21,13 +21,13 @@
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress local file*/
 
-define( 'DB_NAME', 'accelerance' );
+define( 'DB_NAME', 'u287640459_accelerance' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'u287640459_accelerance' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'acceleranceWebsite@123' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
